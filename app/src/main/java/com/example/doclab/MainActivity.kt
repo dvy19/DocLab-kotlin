a49bd49599031f97d16d5877d4c118754f919ee1
dvy19 <divyadixit.311204@gmail.com>
2025-12-21 21:52:27 +0530

    progress ui started

--- app/src/main/java/com/example/doclab/MainActivity.kt
+++ app/src/main/java/com/example/doclab/MainActivity.kt
@@ -1,16 +1,37 @@
 package com.example.doclab
 
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.doclab.ui.theme.DoclabTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +41,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             DoclabTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+
+
+
+
+
                 }
             }
         }
@@ -31,17 +53,76 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun LoginScreen(onLoginSuccess: () -> Unit) {
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    DoclabTheme {
-        Greeting("Android")
+    var username by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    var context=LocalContext.current.applicationContext
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 26.dp, vertical = 140.dp),
+        verticalArrangement = Arrangement.Bottom,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        OutlinedTextField(
+            value = username,
+            onValueChange = { username = it },
+            label = { Text("Username") },
+            shape= RoundedCornerShape(20.dp),
+            leadingIcon = {
+                Icon(imageVector = Icons.Default.Person, contentDescription = "Username")
+            },
+
+            modifier=Modifier.fillMaxWidth()
+                .padding(bottom=8.dp)
+        )
+    }
+
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 26.dp, vertical = 140.dp),
+        verticalArrangement = Arrangement.Bottom,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Username") },
+            shape= RoundedCornerShape(20.dp),
+            leadingIcon = {
+                Icon(imageVector = Icons.Default.Person, contentDescription = "Password")
+            },
+
+            modifier=Modifier.fillMaxWidth()
+                .padding(bottom=8.dp),
+            visualTransformation = PasswordVisualTransformation()
+        )
+
+        Button(onClick = {
+            if(auth(username,password)){
+                onLoginSuccess()
+                Toast.makeText(context,"Done",Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        ) { }
     }
 }
+
+
+
+private fun auth(username:String,password:String):Boolean
+{
+    val validName="amdin"
+    val validPass="123"
+
+    return username==validName && password==validPass
+}
+
+
+
